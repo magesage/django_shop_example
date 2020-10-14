90467f31a32090e92aaf5c6d689d5a8d6375ab5f
Nikolay Emelyanov <ye177@rambler.ru>
2020-10-14 20:36:38 +0300

    Some changes on the page News

--- news/views.py
+++ news/views.py
@@ -4,8 +4,8 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    #news = News.objects.order_by('-created_at').filter(is_published=True)
-    #paginator = Paginator(news, 6)
-    #page_num = request.GET.get('page', 1)
-    #page_objects = paginator.get_page(page_num)
-    return render(request, 'news/index.html',) #context={'page_obj': page_objects})
+    news = News.objects.order_by('-created_at').filter(is_published=True)
+    paginator = Paginator(news, 5)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/index.html', context={'page_obj': page_objects})
